22804cf6114cae1d6a0de695412f060a12ac4b48
Rajeev Kumar Kaushal <rajeevkumarkaushal8563@gmail.com>
2025-09-04 02:15:46 +0530

    Update vite.config.mjs

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: process.cwd(),
+  server: {
+    fs: {
+      allow: [
+        // Search up for workspace root
+        searchForWorkspaceRoot(process.cwd()),
+        // Allow serving files from parent directory
+        '..',
+        // Allow serving files from current directory
+        '.'
+      ]
+    }
+  },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
