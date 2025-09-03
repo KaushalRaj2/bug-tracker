fd13fc6085af6653bd4d22f0298835d45159f14a
Rajeev Kumar Kaushal <rajeevkumarkaushal8563@gmail.com>
2025-09-04 02:34:37 +0530

    Update vite.config.mjs

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      // only allow serving files from the client folder
-      allow: ['.']
-    }
-  },
   build: {
     outDir: 'dist',
     emptyOutDir: true
+  },
+  server: {
+    fs: {
+      allow: ['.'] // only allow current dir
+    }
   }
 })
