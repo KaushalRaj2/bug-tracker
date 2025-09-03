349079fbbdb92934ed27e931114edcc2a5e5712e
Rajeev Kumar Kaushal <rajeevkumarkaushal8563@gmail.com>
2025-09-04 01:43:30 +0530

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,30 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
-      '@services': fileURLToPath(new URL('./src/services', import.meta.url)),
-      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url))
-    }
-  },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-          utils: ['axios', 'date-fns', 'clsx']
-        }
-      }
-    }
+    outDir: 'dist'
   }
 })
